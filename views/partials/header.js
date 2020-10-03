e369c6e0144eaf33b9fb451e1816fdf92090f85b
Kiara <kiaraymg@gmail.com>
2020-10-03 19:17:29 +0200

    Adjusted header for github pages.

--- views/partials/header.js
+++ views/partials/header.js
@@ -1,17 +1,17 @@
 export const Header = {
     render: (loggedUser) => {
         let menu;
-        if (loggedUser) {
+        if (loggedUser && loggedUser.email) {
             menu = `                
                     <li class="nav-item"><a class="nav-link navbar-brand" href="/index.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-link navbar-brand" href="/views/userPage.html">Movies</a></li>
-                    <li id="logout_button" class="nav-item"><a class="nav-link" href="/views/login.html">Logout</a></li>`;
+                    <li class="nav-item"><a class="nav-link navbar-brand" href="/LightFlix2/views/userPage.html">Movies</a></li>
+                    <li id="logout_button" class="nav-item"><a class="nav-link" href="/LightFlix2/views/login.html">Logout</a></li>`;
         } else {
             menu = `
                     <li class="nav-item"><a class="navbar-brand nav-link" href="/index.html">Home</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/views/login.html">Login</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/views/register.html">Register</a></li>
-                    <li class="nav-item"><a class="nav-link" href="/views/help.html">Help</a></li>`
+                    <li class="nav-item"><a class="nav-link" href="/LightFlix2/views/login.html">Login</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/LightFlix2/views/register.html">Register</a></li>
+                    <li class="nav-item"><a class="nav-link" href="/LightFlix2/views/help.html">Help</a></li>`
         }
         return `
             <div class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
