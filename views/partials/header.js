470385ff5e9eeedc84a8541820294c9611ef205e
Kiara <kiaraymg@gmail.com>
2020-10-03 19:32:19 +0200

    Fix.

--- views/partials/header.js
+++ views/partials/header.js
@@ -4,12 +4,12 @@ export const Header = {
         if (loggedUser && loggedUser.email) {
             console.log(loggedUser)
             menu = `                
-                    <li class="nav-item"><a class="nav-link navbar-brand" href="/index.html">Home</a></li>
+                    <li class="nav-item"><a class="nav-link navbar-brand" href="/LightFlix2/index.html">Home</a></li>
                     <li class="nav-item"><a class="nav-link navbar-brand" href="/LightFlix2/views/userPage.html">Movies</a></li>
                     <li id="logout_button" class="nav-item"><a class="nav-link" href="/LightFlix2/views/login.html">Logout</a></li>`;
         } else {
             menu = `
-                    <li class="nav-item"><a class="navbar-brand nav-link" href="/index.html">Home</a></li>
+                    <li class="nav-item"><a class="navbar-brand nav-link" href="/LightFlix2/index.html">Home</a></li>
                     <li class="nav-item"><a class="nav-link" href="/LightFlix2/views/login.html">Login</a></li>
                     <li class="nav-item"><a class="nav-link" href="/LightFlix2/views/register.html">Register</a></li>
                     <li class="nav-item"><a class="nav-link" href="/LightFlix2/views/help.html">Help</a></li>`
